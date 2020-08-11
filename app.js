ac1451f972f0514a03f71ab71dca09d922b6bd15
Germán Escobar <german.escobarc@gmail.com>
2020-08-10 20:23:15 -0500

    Versión inicial

---
+++ app.js
@@ -0,0 +1,12 @@
+function esMayorDeEdad(edad) {
+  if (edad >= 18) { // si la edad es igual o mayor a 18
+    console.log(edad + " es mayor de edad") // imprima Es mayor de edad
+  } else { // de lo contrario
+    console.log(edad + " es menor de edad") // imprima Es menor de edad
+  }
+}
+
+esMayorDeEdad(17)
+esMayorDeEdad(58)
+esMayorDeEdad(18)
+esMayorDeEdad(72)
